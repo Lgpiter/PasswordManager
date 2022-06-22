a8e915685d075a4dd15d2db17ba1c3d9c2d429ef
Lgpiter <piotr.zadykowicz@wp.pl>
2022-06-22 14:06:04 +0200

    Initial commit

---
+++ functions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Piotr Zadykowicz on 22.06.2022.
+//
+
+#include "functions.h"
---
+++ functions.h
@@ -0,0 +1,14 @@
+//
+// Created by Piotr Zadykowicz on 22.06.2022.
+//
+
+#ifndef PASSWORDMANAGER_FUNCTIONS_H
+#define PASSWORDMANAGER_FUNCTIONS_H
+
+
+class functions {
+
+};
+
+
+#endif //PASSWORDMANAGER_FUNCTIONS_H
